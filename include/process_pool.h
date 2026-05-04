38065b9bcc3beb4db7b7c6315545ac03a68e5f98
soroush <soroushsoleimani1s@gmail.com>
2026-05-04 15:25:08 +0330

    edit process pool file child process

--- include/process_pool.h
+++ include/process_pool.h
@@ -2,12 +2,15 @@
 #define PROCESS_POOL_H
 
 #include <sys/types.h>
-#include <stdbool.h>
+#include <stdio.h>   
 
-typedef struct child_process child_process_t;
+typedef struct child_process {
+    pid_t pid;
+    int pipe_fd;
+} child_process_t;
 
 int create_child_process(const char *command, int pipe_write_end, pid_t *pid);
-void reap_finished_processes(child_process_t *children, int *active_count, void *log_file);
-void wait_for_all_children(child_process_t *children, int active_count, void *log_file);
+void reap_finished_processes(child_process_t *children, int *active_count, FILE *log_file);
+void wait_for_all_children(child_process_t *children, int active_count, FILE *log_file);
 
 #endif
